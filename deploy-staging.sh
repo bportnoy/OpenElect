58af1ad0b41b67a7203caa49396a2b46ac680c15
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-04-08 21:15:57 -0700

    Final touches on deployment script.

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #SkillfulCactus AWS Deployment Script
 
+echo "Beginning deployment procedure."
+
 #Install Elastic Beanstalk command line tools.
 sudo pip install awsebcli
 
@@ -12,9 +14,14 @@ grunt build
 
 #Move to distribution directory
 cd dist
+echo "Now in:"
+pwd
+echo "Preparing to upload to Elastic Beanstalk."
 
 #Initialize eb instance to our app.
 eb init -r us-west-1 SkillfulCactus
 
 #Deploy directory to staging.
 eb deploy skillfulcactus-staging
+
+echo "Deployment complete, have a prickly day!"
