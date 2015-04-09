a4664384b45986d87a797c6ac6ac96ada9684003
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-04-08 20:49:12 -0700

    Update deployment script.

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,7 +1,20 @@
-###SkillfulCactus AWS Deployment Script
-grunt build
+#!/bin/bash
+#SkillfulCactus AWS Deployment Script
+
+#Install Elastic Beanstalk command line tools.
 sudo pip install awsebcli
+
+#Ensure we're in the master directory
 git checkout master
+
+# Build distribution directory
+grunt build
+
+#Move to distribution directory
 cd dist
+
+#Initialize eb instance to our app.
 eb init -r us-west-1 SkillfulCactus
+
+#Deploy directory to staging.
 eb deploy skillfulcactus-staging
