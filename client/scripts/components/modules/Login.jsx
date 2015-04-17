0c7395bbcea8a128c242021631209201bd8d879c
rebekahkwon <rebekah.kwon@gmail.com>
2015-04-16 19:51:18 -0700

    implement user signup and register form validation

--- client/scripts/components/modules/Login.jsx
+++ client/scripts/components/modules/Login.jsx
@@ -2,21 +2,24 @@
 
 var React = require('react/addons');
 var axios = require('axios');
+var ValidationMixin = require('react-validation-mixin');
+var Joi = require('joi');
 
 var Login = React.createClass({
-
-  mixins: [React.addons.LinkedStateMixin],
-
-  contextTypes: {
-    router: React.PropTypes.func
-  },
 
   // componentDidMount: function() {
   //   //something will go here
   // },
 
-  // getInitialState: function() {
-  //   //something will go here
+  mixins: [ValidationMixin, React.addons.LinkedStateMixin],
+
+  contextTypes: {
+    router: React.PropTypes.func
+  },
+
+  // validatorTypes: {
+  //   email: Joi.string().required().label('E-mail Address'),
+  //   password: Joi.string().required().label('Password')
   // },
 
   getInitialState: function() {
@@ -28,7 +31,7 @@ var Login = React.createClass({
     };
   },
 
-  submit: function (e) {
+  handleSubmit: function (e) {
     this.setState({spinner: true});
     e.preventDefault();
 
@@ -46,6 +49,19 @@ var Login = React.createClass({
 
   },
 
+  getClasses: function(field) {
+    return React.addons.classSet({
+      'form-group': true,
+      'has-error': !this.isValid(field)
+    });
+  },
+
+  renderHelpText: function(message) {
+    return (
+      <span className='help-block'>{message}</span>
+      );
+  },
+
   render: function() {
     var loginWarning = this.state.failedLogins > 0 ? (<span className='warning'>Username or password incorrect</span>) : undefined;
     var spinner = this.state.spinner > 0 ? (<i className="fa fa-circle-o-notch fa-spin fa-2x"></i>) : undefined;
@@ -53,13 +69,17 @@ var Login = React.createClass({
       <div className='login'>
         <h1>Login</h1>
         <form>
-          <label htmlFor='email'>E-mail Address</label>
-          <input type='email' id='email' placeholder='E-mail' valueLink={this.linkState('email')}/>
-          <br/>
-          <label htmlFor='password'>Password</label>
-          <input type='password' id='password' placeholder='Password' valueLink={this.linkState('password')}/>
-          <br/>
-          <button type='submit' onClick={this.submit}>Submit</button><div>{spinner}</div>
+          <div className={this.getClasses('email')}>
+            <label htmlFor='email'>E-mail Address</label>
+            <input type='email' id='email' placeholder='E-mail' onBlur={this.handleValidation('email')} valueLink={this.linkState('email')}/>
+            {this.getValidationMessages('email').map(this.renderHelpText)}
+          </div><br/>
+          <div className={this.getClasses('password')}>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' placeholder='Password' onBlur={this.handleValidation('password')} valueLink={this.linkState('password')}/>
+            {this.getValidationMessages('password').map(this.renderHelpText)}
+          </div><br/>
+            <button type='submit' onClick={this.handleSubmit}>Submit</button><div>{spinner}</div>
         </form>
         <div>{loginWarning}</div>
       </div>
--- client/scripts/components/modules/Signup.jsx
+++ client/scripts/components/modules/Signup.jsx
@@ -2,8 +2,10 @@
 
 var React = require('react/addons');
 var axios = require('axios');
+var ValidationMixin = require('react-validation-mixin');
+var Joi = require('joi'); // replace with joi-react shim
 
-var Signup = React.createClass({
+var SignUp = React.createClass({
 
   mixins: [React.addons.LinkedStateMixin],
 
@@ -11,8 +13,17 @@ var Signup = React.createClass({
   //   //something will go here
   // },
 
-  // getInitialState: function() {
-  //   //something will go here
+  mixins: [ValidationMixin, React.addons.LinkedStateMixin],
+
+  contextTypes: {
+    router: React.PropTypes.func
+  },
+
+  // validatorTypes: {
+  //   first_name: Joi.string().required().label('First Name'),
+  //   last_name: Joi.string().required().label('Last Name'),
+  //   email: Joi.string().email().required().label('E-mail Address'),
+  //   password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).required().label('Password')
   // },
 
   getInitialState: function() {
@@ -24,40 +35,63 @@ var Signup = React.createClass({
     };
   },
 
-  submit: function (e) {
+  handleSubmit: function (e) {
 
     e.preventDefault();
 
     axios.post('/api/v1/users/signup',{
       user: this.state
     }).then(function(response){
-      console.log(response);
+      this.context.router.transtionTo(response.data);
     });
 
   },
 
+  getClasses: function(field) {
+    return React.addons.classSet({
+      'form-group': true,
+      'has-error': !this.isValid(field)
+    });
+  },
+
+  renderHelpText: function(message) {
+    return (
+      <span className='help-block'>{message}</span>
+      );
+  },
+
   render: function() {
+    console.log(Joi);
     return (
       <div className='signup'>
         <h1>Sign up for Open Elect</h1>
         <form>
-          <label htmlFor='firstName'>First Name</label>
-          <input type='text' id='firstName' placeholder='Franklin' valueLink={this.linkState('first_name')}/>
-          <br/>
-          <label htmlFor='lastName'>Last Name</label>
-          <input type='text' id='lastName' placeholder='Roosevelt' valueLink={this.linkState('last_name')}/>
-          <br/>
-          <label htmlFor='email'>E-mail Address</label>
-          <input type='email' id='email' placeholder='fdr@whitehouse.gov' valueLink={this.linkState('email')}/>
-          <br/>
-          <label htmlFor='password'>Password</label>
-          <input type='password' id='password' valueLink={this.linkState('password')}/>
-          <br/>
-          <button type='submit' onClick={this.submit}>Submit</button>
+          <div className={this.getClasses('firstName')}>
+            <label htmlFor='firstName'>First Name</label>
+            <input type='text' id='firstName' placeholder='Franklin' valueLink={this.linkState('first_name')}/>
+            {this.getValidationMessages('firstName').map(this.renderHelpText)}
+          </div><br/>
+          <div className={this.getClasses('lastName')}>
+            <label htmlFor='lastName'>Last Name</label>
+            <input type='text' id='lastName' placeholder='Roosevelt' valueLink={this.linkState('last_name')}/>
+            {this.getValidationMessages('lastName').map(this.renderHelpText)}
+          </div><br/>
+          <div className={this.getClasses('email')}>
+            <label htmlFor='email'>E-mail Address</label>
+            <input type='email' id='email' placeholder='fdr@whitehouse.gov' valueLink={this.linkState('email')}/>
+            {this.getValidationMessages('email').map(this.renderHelpText)}
+          </div><br/>
+          <div className={this.getClasses('password')}>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' valueLink={this.linkState('password')}/>
+            {this.getValidationMessages('password').map(this.renderHelpText)}
+          </div><br/>
+            <button type='submit' onClick={this.handleSubmit}>Submit</button>
         </form>
       </div>
     );
   },
 });
 
-module.exports = Signup;
+
+module.exports = SignUp;
