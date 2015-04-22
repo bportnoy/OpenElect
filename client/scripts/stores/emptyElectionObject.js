371bcf39b61c063f2fa155aecf92e12e1a478bc5
Ryan Carey <me@ryancarey.ca>
2015-04-22 11:27:59 -0700

    add missing files

---
+++ client/scripts/stores/emptyElectionObject.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var election = {
+  'id': null,
+  'owner_id': null,
+  'name': null,
+  'description': null,
+  'start': null,
+  'end': null,
+  'timed': null,
+  'accepting_votes': null,
+  'locked': null,
+  'privacy_strategy': null,
+  'url_handle': null,
+  'randomize_answer_order': null,
+  'two_factor_auth': null,
+  'force_two_factor_auth': null,
+  'public_key': null,
+  'results': null,
+  'user_time_zone': null,
+  'created_at': null,
+  'updated_at': null
+};
+
+module.exports = election;
