d90a979cda74ccebbb9af63211cf50a8f40c5394
brianpchsu <brianpchsu@gmail.com>
2015-04-23 23:23:53 -0700

    Fix #157 Poll get and update method

--- server/controllers/api/polls.js
+++ server/controllers/api/polls.js
@@ -6,6 +6,7 @@
 
 var Poll = require('../../database/models/poll');
 var uuid = require('uuid');
+var _ = require('lodash');
 
 var polls = {
 
@@ -23,6 +24,8 @@ var polls = {
         res.status(201);
         res.json({
           id: model.get('id'),
+          election_id: model.get('election_id'),
+          group_id: model.get('group_id'),
           name: model.get('name'),
           created_at: model.get('created_at')
         });
@@ -39,12 +42,57 @@ var polls = {
 	},
 
 	adminGetById: function ( id, req, res ) {
-
+    if( req.body){
+      var data = req.body;
+      var poll = new Poll({election_id: id})
+          .fetch()
+          .then(function(model){
+            res.status(201);
+            res.json({
+              id: model.get('id'),
+              election_id: model.get('election_id'),
+              group_id: model.get('group_id'),
+              name: model.get('name'),
+              created_at: model.get('created_at')
+            });
+            res.end();
+        }).error(function(error){
+          res.status(500);
+          console.error(error);
+          res.end();
+        });
+      } else {
+      res.status(400);
+      res.end('Bad request');
+    }
 	},
 
 	updateById: function ( id, req, res ) {
-
-	}
+    var data = req.body;
+    var poll = new Poll({election_id: id});
+    poll.fetch()
+      .then(function(poll){
+        if(poll){
+          _(data).forEach(function(value, property){
+            // check to make sure we aren't allowing admins to change important stuff
+            if (  property !== 'id'
+                  && property !== 'created_at'
+                  && property !== 'updated_at'
+                  && property !== 'election_id'
+                  && property !== 'group_id'
+                ) {
+                    poll.set(property, value);
+                }
+          });
+          poll.save().then(function(poll){
+            res.send(poll.toJSON());
+          });
+        } else {
+          res.status(404);
+          res.end('Poll object not found');
+        }
+      });
+  },
 
 };
 
