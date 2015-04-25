d8ac10df3cf8332711270e4b7ecef5d02eefdf18
brianpchsu <brianpchsu@gmail.com>
2015-04-24 20:28:29 -0700

    Fix election voting viewing based on accepting_votes

--- client/scripts/components/modules/vote/VotingBooth.jsx
+++ client/scripts/components/modules/vote/VotingBooth.jsx
@@ -25,22 +25,45 @@ var VotingBooth = React.createClass({
     var userId = 1;
     BallotActions.setUserAndElection(userId, electionId);
 
-    axios.get('/api/v1/elections/vote/' + electionId)
-          .then(function(response){
-            this.setState({polls: response.data});
-          }.bind(this))
-          .catch(function(response){
-            console.error(response);
-          });
+    axios.get('api/v1/elections/update/' + electionId)
+      .then(function(response){
+        // if currently accepting vote, then show polls
+        if(response.data.accepting_votes){
+          axios.get('/api/v1/elections/vote/' + electionId)
+            .then(function(election){
+              this.setState({polls: election.data});
+            }.bind(this))
+            .catch(function(election){
+              console.error(election);
+            });
+        } else {
+          // Get election start and end day to show?
+          this.setState({polls: "not_ready"});
+        }
+      }.bind(this))
+      .catch(function(response){
+        console.error(response);
+      });
+
   },
 
   render: function() {
-    if (this.state.polls === null) return (<Spinner/>);
+    // Check the state to decide show the right messages
+    if (this.state.polls === null) {
+      // Beginner state
+      var display = (<Spinner/>);
+    } else if (this.state.polls ==="not_ready"){
+      var display = (<div>This election is not currently accepting votes.</div>);
+    } else {
+      var display = (
+        <div>
+          <Election polls={this.state.polls}/>
+        </div>
+      )
+    }
 
     return (
-      <div>
-        <Election polls={this.state.polls}/>
-      </div>
+      <div>{display}</div>
     );
   },
 
