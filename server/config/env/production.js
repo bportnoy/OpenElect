f5aa418f09c720da100b45ade04fcff1a9d74daf
Ryan Carey <me@ryancarey.ca>
2015-04-26 12:12:06 -0700

    update db config to use rds in production

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -5,12 +5,12 @@
 
 // Production specific configuration
 var prodConfig = {
-  logLevel: 'short',
+  logLevel: 'dev',
   NODE_ENV: 'production',
   DB_HOST: process.env.RDS_HOSTNAME,
   DB_USER: process.env.RDS_USERNAME,
   DB_PASSWORD: process.env.RDS_PASSWORD,
-  DB_NAME: "openelect",
+  DB_NAME: "ebdb",
   PORT: process.env.RDS_PORT
 };
 
